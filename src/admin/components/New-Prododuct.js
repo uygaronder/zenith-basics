dd1c11990fa8656535b096d9032cf51186fea13b
uygaronder <uonder50@gmail.com>
2023-09-08 00:35:00 +0300

    product add page styling

--- src/admin/components/New-Prododuct.js
+++ src/admin/components/New-Prododuct.js
@@ -2,33 +2,58 @@ import React from 'react'
 
 import "../css/newProduct.css"
 
-import Image from "../res/image-regular.svg"
+import Upload from "../res/upload.svg"
 
 function NewPrododuct() {
   return (
     <section id='newProduct'>
-        <div id='imagesContainer'>
-            <span className='addImage'><img src={Image} /> <p>New Image</p></span>
-            <span id='images'>
-                <span className='img'>
-                    <img src='https://placehold.co/100x100' />
+        <div id='productSettings'>
+            <div id='images'>
+                <h2>Images</h2>
+                <div id='imagesInput'>
+                    <span className='imageInput'>
+                        <label htmlFor='imageInput'>
+                            <img src={Upload} alt='image' />
+                            <p><b>Choose file</b> or drag it here.</p>
+                        </label>
+                        <input id='imageInput' type='file' />
+                    </span>
+                </div>
+                <span id='uploadedImages'>
+                    <span className='uploadedImage'>
+                        <img src='https://picsum.photos/200' />
+                    </span>
+                    <span className='uploadedImage'>
+                        <img src='https://picsum.photos/200' />
+                    </span>
+                    <span className='uploadedImage'>
+                        <img src='https://picsum.photos/200' />
+                    </span>
+                    <span className='uploadedImage'>
+                        <img src='https://picsum.photos/200' />
+                    </span>
+                    <span className='uploadedImage'>
+                        <img src='https://picsum.photos/200' />
+                    </span>
                 </span>
-                <span className='img'>
-                    <img src='https://placehold.co/100x100' />
+            </div>
+            <span id='aboutItem'>
+                <h2>About Item</h2>
+                <span className='aboutItemContainer'>
+                    <span className='aboutItemInput'>
+                        <input type='text' />
+                    </span>
                 </span>
-                <span className='img'>
-                    <img src='https://placehold.co/100x100' />
-                </span>
-                <span className='img'>
-                    <img src='https://placehold.co/100x100' />
-                </span>
-                <span className='img'>
-                    <img src='https://placehold.co/100x100' />
+                <span className='aboutItemContainer'>
+                    <span className='aboutItemInput'>
+                        <input type='text' />
+                    </span>
                 </span>
             </span>
-        </div>
-        <div className='imageBig'>
-            <img src='https://placehold.co/300x300' />
+            <span id='description'>
+                <h2>Description</h2>
+                <textarea placeholder='Description'></textarea>
+            </span>
         </div>
         <div id='productInfo'>
             <span className='productInputContainer productTextInputContainer'>
@@ -37,15 +62,73 @@ function NewPrododuct() {
                     <input id='productName' type='text' />
                 </span>
             </span>
-            <span className='productInputContainer priceInputContainer'>
-                <label htmlFor='productPrice'>Product Price</label>
+            <span className='categoryInputContainer'>
+                <label htmlFor='productCategory'>Product Category</label>
                 <span className='inputSpan'>
-                    <span className='currency'>$</span>
-                    <input id='productPrice' type='number' />
+                    <select id='productCategory'>
+                        <option value=''>Select Category</option>
+                        <option value=''>Category 1</option>
+                        <option value=''>Category 2</option>
+                        <option value=''>Category 3</option>
+                    </select>
                 </span>
             </span>
-            <span id='specifications'>
-                <span className='specification'>
+            <span className='productInputContainer priceInputContainer'>
+                <div>
+                    <label htmlFor='productPrice'>Product Price</label>
+                    <span className='inputSpan'>
+                        <span className='currency'>$</span>
+                        <input id='productPrice' type='number' />
+                    </span>
+                </div>
+                <div>
+                    <label htmlFor='productDiscount'>Discount Price</label>
+                    <span className='inputSpan'>
+                        <span className='currency'>$</span>
+                        <input id='productDiscount' type='number' />
+                    </span>
+                </div>
+            </span>
+            <span id='stockContainer'>
+                <div>
+                    <label htmlFor='stockStatus'>Stock Status</label>
+                    <span className='inputSpan'>
+                        <select id='stockStatus'>
+                            <option value=''>In Stock</option>
+                            <option value=''>Out of Stock</option>
+                        </select>
+                    </span>
+                </div>
+                <div>
+                    <label htmlFor='stockQuantity'>Stock Quantity</label>
+                    <span className='inputSpan'>
+                        <input id='stockQuantity' type='number' />
+                    </span>
+                </div>
+            </span>
+            <span id='availableSizesContainer'>
+                <h2>Available Sizes</h2>
+                <span className='sizesSpan'>
+                    <div className='availableSizeCheck'>
+                        <input id='availableSizes' type='checkbox' />
+                        <label htmlFor='availableSizes'>S</label>
+                    </div>
+                    <div className='availableSizeCheck'>
+                        <input id='availableSizes' type='checkbox' />
+                        <label htmlFor='availableSizes'>M</label>
+                    </div>
+                    <div className='availableSizeCheck'>
+                        <input id='availableSizes' type='checkbox' />
+                        <label htmlFor='availableSizes'>L</label>
+                    </div>
+                    <div className='availableSizeCheck'>
+                        <input id='availableSizes' type='checkbox' />
+                        <label htmlFor='availableSizes'>XL</label>
+                    </div>
+                    <div className='availableSizeCheck'>
+                        <input id='availableSizes' type='checkbox' />
+                        <label htmlFor='availableSizes'>XXL</label>
+                    </div>
                 </span>
             </span>
             {/* 
